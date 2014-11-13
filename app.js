15d8d6c7a3ca83e502befd65d83ea00e8d30d2aa
icg-ejohnson <ejohnson@icginfo.com>
2014-11-13 11:48:31 -0800

    complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 angular.module('testApp', ['ngResource'])
-    .factory('tasks', ['$resource', '$q', function ($resource, $q) {
+    .factory('sequences', ['$resource', '$q', function ($resource, $q) {
 
         var r = $resource('https://gist.githubusercontent.com/thireven/841b585de43a7d708bff/raw/abe8f8ceacff3817fb3ee5bf8219408819563629/sequence_sample.json',
             null,
@@ -9,80 +9,154 @@ angular.module('testApp', ['ngResource'])
 
         return function () {
             var self = {};
+            var sequences = null;
+            var data = null;
 
-            self.get = function () {
+            self.load = function(){
                 var d = $q.defer();
 
-                r.get().$promise.then(function(data){
-                    var sequences = data[0].sequences;
-                    var t = [];
-                    console.log("sequences : ", sequences);
-                    for(var i=0; i<sequences.length; i++){
-                        var seq = sequences[i];
-                        console.log("task : ", seq.tasks);
-                        if(seq.tasks != undefined){
-                            t = t.concat(seq.tasks);
-                        }
-                    }
+                r.get().$promise.then(function (res) {
+                    data = res[0];
+                    sequences = data.sequences;
 
-                    d.resolve(t);
+                    d.resolve();
                 });
 
                 return d.promise;
             }
 
+            self.getData = function(){
+                return data;
+            }
+
+            self.getTasks = function (seqenceId) {
+                var t = [];
+
+                if(seqenceId){
+                    var seq = self.getSequence(seqenceId);
+                    t = seq.tasks;
+                }else{
+                    //get all tasks
+                    for (var i = 0; i < sequences.length; i++) {
+                        var seq = sequences[i];
+                        if (seq.tasks != undefined) {
+                            t = t.concat(seq.tasks);
+                        }
+                    }
+                }
+
+                return t;
+            }
+
+            self.getSequence = function(id){
+                var sId = (id)?id:false;
+                var r = sequences;
+                if(sId){
+                    r = _.findWhere(sequences, {"id":id});
+                }
+                return r;
+            }
+
             return self;
         }();
     }])
-    .controller('mainController', ['$parse', '$sce', '$scope', '$timeout', 'tasks', function ($parse, $sce, $scope, $timeout, tasks) {
+    .controller('mainController', ['$document', '$parse', '$sce', '$scope', '$timeout', 'sequences', function ($document, $parse, $sce, $scope, $timeout, sequences) {
+        var editor = null;
+        $document.ready(function(){
+            editor= ace.edit("editor");
 
-        var editor = ace.edit("editor");
+            editor.setTheme("ace/theme/monokai");
+            editor.getSession().setMode("ace/mode/text");
+
+            editor.getSession().on('change', function () {
+                if(editor.getValue().length == 0){
+                    return;
+                }
+                //check if the last character is a new line
+                if (editor.getValue().lastIndexOf("\n") === editor.getValue().length - 1) {
+                    //when the type is info go to the next task
+                    if (currentTask.options.type == 'info') {
+                        nextTask();
+                    } else if (currentTask.isValid) {
+                        nextTask();
+                    } else {
+                        validateLines();
+                    }
+                } else {
+                    validateLines();
+                }
+            });
+
+            nextTask();
+        })
         var currentTask = null;
 
+        $scope.sequences = [];
         $scope.tasks = [];
-
         $scope.infoContainerContent = "";
-
         $scope.taskType = 'info';
+        $scope.btnHighlightClass = 'btn-success';
+        $scope.lines = null;
+        $scope.title = "";
+        $scope.infoTitle = "";
 
-        tasks.get().then(function(data){
-            $scope.tasks = data;
+        Object.defineProperty($scope, 'infoTitle',{
+            get : function(){
+                return (currentTask.options.type == 'info')?"Instructions":"Lession Board";
+            }
         });
 
-        $scope.lines = null;
+        sequences.load().then(function () {
+            $scope.title = sequences.getData().name;
+            $scope.tasks = sequences.getTasks();
 
-        $scope.taskClicked = function(task){
+            $scope.sequences = sequences.getSequence();
+        });
+
+
+
+        $scope.taskClicked = function (task) {
             $scope.showTask(task);
         }
 
-        $scope.showTask = function(task){
-            currentTask = task;
+        $scope.showTask = function (task) {
+            $scope.currentTask = currentTask = task;
 
             $scope.taskType = task.options.type;
 
-            console.log("task.options.type : ", task.options.type);
-            if(task.options.type == 'info'){
+            if (task.options.type == 'info') {
                 $scope.lines = $sce.trustAsHtml(task.lines);
-            }else{
+            } else {
                 $scope.lines = [];
-                _.each(task.lines, function(line){
+                _.each(task.lines, function (line, index) {
                     var l = $sce.trustAsHtml(line);
-                    $scope.lines.push({"html":l, "raw":line, "class":"invalid-text"});
+                    var obj = {
+                        "trustedHtml": l,
+                        "raw": line,
+                        "class": "invalid-text",
+                        "valid": false,
+                        "show": true
+                    };
+                    if (index > 0) {
+                        obj.show = false;
+                    }
+                    $scope.lines.push(obj);
                 });
             }
 
             editor.focus();
         }
 
-        var nextTask = function(){
-            tasks.get().then(function(){
+        var nextTask = function () {
+            sequences.load().then(function () {
                 var index = _.indexOf($scope.tasks, currentTask);
-                var nextIndex = (index == -1)?0:index+1;
-                if(nextIndex >= $scope.tasks.length){
+                var nextIndex = (index == -1) ? 0 : index + 1;
+                if (nextIndex >= $scope.tasks.length) {
                     nextIndex = 0;
+                    $('#myModal').modal({});
                 }
 
-                $timeout(function(){
+                $timeout(function () {
                     $scope.showTask($scope.tasks[nextIndex]);
                 });
 
@@ -90,44 +164,46 @@ angular.module('testApp', ['ngResource'])
             });
         }
 
-        var validateLines = function(){
+        var validateLines = function () {
             var rows = editor.getSession().getLength();
+            var amountValid = 0;
 
-            for(var i=0; i<rows; i++){
+            for(var i = 0; i<rows; i++){
                 var inputLineContent = editor.getSession().getLine(i);
-                if(inputLineContent == ""){
-                    continue;
+                if (inputLineContent == "") {
+                    return true;
                 }
-                var lineObj = $scope.lines[i];
+                //retrieve incoded html from the DOM
+                $scope.lines[i].html = _.unescape($('#info ol li').eq(i).html());
 
-                console.log("unexsvape : ", unescape(lineObj.raw));
-
-                if(inputLineContent === lineObj.html){
+                var nextLineIndex = i+1;
+                if (inputLineContent === $scope.lines[i].html) {
                     $scope.lines[i].class = "valid-text";
-                }else{
+                    $scope.lines[i].valid = true;
+                    amountValid = (amountValid+1 < $scope.lines.length)?amountValid+1:$scope.lines.length;
+                    if(nextLineIndex < $scope.lines.length){
+                        defineShowByIndex(nextLineIndex, true);
+                    }
+                } else {
                     $scope.lines[i].class = "invalid-text";
-                    console.log("$scope.lines[i].class : " , $scope.lines[i].class);
+                    $scope.lines[i].valid = false;
+                    amountValid = (amountValid-1 > 0)?amountValid-1:0;
+                    if(nextLineIndex < $scope.lines.length){
+                        defineShowByIndex(nextLineIndex, false);
+                    }
                 }
+                $scope.$digest();
+            };
+
+            if(amountValid == $scope.lines.length){
+                currentTask.isValid = true;
+            }else{
+                currentTask.isValid = false;
             }
         }
 
-        editor.setTheme("ace/theme/monokai");
-        editor.getSession().setMode("ace/mode/text");
-
-        editor.getSession().on('change', function(){
-            if(editor.getValue().indexOf("\n") === editor.getValue().length-1){
-
-                if(currentTask.options.type == 'info'){
-                    nextTask();
-                }else if(currentTask.isValid){
-                    nextTasks();
-                }else{
-                    validateLines();
-                }
-//                console.log("new line");
-//                console.log("editor.getValue() : ", editor.getValue().length);
-            }
-        });
-
-        nextTask();
+        var defineShowByIndex = function(i, val){
+            $scope.lines[i].show = val;
+            $scope.$digest();
+        }
     }]);
